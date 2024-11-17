4620c0d235c97fce542d1baed7254782db1f758d
Joseph Daniel Moreno <joejetmoreno@gmail.com>
2024-11-17 17:39:58 -0500

    Testing github action for gh page

--- profile/src/main.tsx
+++ profile/src/main.tsx
@@ -3,14 +3,7 @@ import { createRoot } from 'react-dom/client'
 import '@mantine/core/styles.css';
 import App from './App.tsx';
 
-// import OldApp from './old/src/App';
-// import useNavbar from './stores/NavbarStore';
-
-// const ver: boolean = useNavbar((state: any) => state.old_ver);
-
 createRoot(document.getElementById('root')!).render(
-  // <MantineProvider theme={DEFAULT_THEME}>
     <App />
-  // </MantineProvider>
   ,
 );
