7de664fa819b1b9a05979dc8be98cd114e8acddd
Joseph Daniel Moreno <joejetmoreno@gmail.com>
2024-12-02 20:34:39 -0500

    Added loader to CanvasChip8

--- src/components/CanvasChip8.tsx
+++ src/components/CanvasChip8.tsx
@@ -1,14 +1,19 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { Loader } from '@mantine/core';
 
 // -- @ts-ignore
 import Chip8 from './emscripten/chip8.mjs';
 
 const CanvasChip8 = ({ opened }: { opened: boolean }) => {
     const normalQuit = useRef<any>(null);
+    const [loading, setLoading] = useState<boolean>(false);
 
     useEffect(() => {
+        setLoading(true);
+
         Chip8({
             canvas: (() => document.getElementById('canvas'))(),
+            onRuntimeInitialized: (() => setLoading(false)),
         }).then((instance: any) => { normalQuit.current = instance.cwrap('normalQuit', null); }); // Under "Interacting with an API written in C/C++ from NodeJS" section: https://emscripten.org/docs/porting/connecting_cpp_and_javascript/Interacting-with-code.html#calling-compiled-c-functions-from-javascript-using-ccall-cwrap
     }, []);
 
@@ -17,7 +22,23 @@ const CanvasChip8 = ({ opened }: { opened: boolean }) => {
             normalQuit.current();
     }, [opened]);
 
-    return(<canvas id='canvas' />);
+    return(
+        <div
+            style={{
+                width: '100%',
+                height: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            {loading
+                ?   <Loader color='blue' />
+                :   <></>
+            }
+            <canvas id='canvas' />
+        </div>
+    );
 };
 
 export default CanvasChip8;
